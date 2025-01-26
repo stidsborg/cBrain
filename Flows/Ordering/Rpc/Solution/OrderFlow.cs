e51058a4d47442041c90d751a2a8d9881cca4584
stidsborg <stidsborg@cleipnir.net>
2025-01-26 08:44:29 +0100

    Added solution to RPC OrderFlow

---
+++ Flows/Ordering/Rpc/Solution/OrderFlow.cs
@@ -0,0 +1,59 @@
+﻿using cBrain.Flows.Batch.Clients;
+using Cleipnir.Flows;
+using Cleipnir.ResilientFunctions.Domain;
+
+namespace cBrain.Flows.Ordering.Rpc.Solution;
+
+public class OrderFlow(
+    IPaymentProviderClient paymentProviderClient,
+    IEmailClient emailClient,
+    ILogisticsClient logisticsClient)
+    : Flow<Order>
+{
+   public override async Task Run(Order order)
+    {
+        var transactionId = await Capture(Guid.NewGuid);
+
+        try { await paymentProviderClient.Reserve(transactionId, order.CustomerId, order.TotalPrice); }
+        catch (FatalWorkflowException) { await CleanUp(FailedAt.FundsReserved, transactionId, trackAndTrace: null); throw; }
+
+        TrackAndTrace? trackAndTrace;
+        try { trackAndTrace = await logisticsClient.ShipProducts(order.CustomerId, order.ProductIds); }
+        catch (FatalWorkflowException) { await CleanUp(FailedAt.FundsReserved, transactionId, trackAndTrace: null); throw; }
+        
+        try { await paymentProviderClient.Capture(transactionId); }
+        catch (FatalWorkflowException) { await CleanUp(FailedAt.FundsReserved, transactionId, trackAndTrace: null); throw; }
+        
+        try { await emailClient.SendOrderConfirmation(order.CustomerId, trackAndTrace!, order.ProductIds); }
+        catch (FatalWorkflowException) { await CleanUp(FailedAt.FundsReserved, transactionId, trackAndTrace: null); throw; }
+    }
+
+    private async Task CleanUp(FailedAt failedAt, Guid transactionId, TrackAndTrace? trackAndTrace)
+    {
+        switch (failedAt) 
+        {
+            case FailedAt.FundsReserved:
+                break;
+            case FailedAt.ProductsShipped:
+                await paymentProviderClient.CancelReservation(transactionId);
+                break;
+            case FailedAt.FundsCaptured:
+                await paymentProviderClient.CancelReservation(transactionId);
+                await logisticsClient.CancelShipment(trackAndTrace!);
+                break;
+            case FailedAt.OrderConfirmationEmailSent:
+                await paymentProviderClient.Reverse(transactionId);
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(failedAt), failedAt, null);
+        }
+    }
+    
+    private enum FailedAt
+    {
+        FundsReserved,
+        ProductsShipped,
+        FundsCaptured,
+        OrderConfirmationEmailSent,
+    }
+}
