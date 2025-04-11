339eaeb5c499fbde628bbf69d29474dbfbf37432
stidsborg <stidsborg@cleipnir.net>
2025-04-11 16:58:16 +0200

    Added SelfScheduleFlow

---
+++ Flows/SelfSchedule/SelfScheduleFlow.cs
@@ -0,0 +1,17 @@
+﻿using Cleipnir.Flows;
+
+namespace cBrain.Flows.SelfSchedule;
+
+[GenerateFlows]
+public class SelfScheduleFlow(SelfScheduleFlows flows) : Flow<int>
+{
+    
+    public override async Task Run(int param)
+    {
+        await DoWork();
+
+        await flows.ScheduleIn($"InstanceId#{param + 1}", param + 1, TimeSpan.FromSeconds(5));
+    }
+
+    private Task DoWork() => Task.Delay(100);
+}
